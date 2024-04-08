d3c5952bf7015b1c2e9004300a629e100424f7b6
raymondytian <rymdtian@gmail.com>
2024-04-07 23:39:04 -0500

    feat: split image functionality
    Only tested if  returns correct number of images. Many efficiency improvements to be made.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
-use image;
+use image::*;
+use std::cmp;
 use std::path::PathBuf;
 
 #[derive(Parser)]
@@ -15,6 +16,20 @@ struct Cli {
     /// Number of columns
     #[arg(short, long)]
     cols: Option<u32>,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    if let Err(err) = validate_args(&cli) {
+        eprintln!("{}", err);
+    }
+
+    let img_path = cli.image;
+    let rows = cli.rows.unwrap_or(1);
+    let cols = cli.cols.unwrap_or(1);
+
+    split_image(&img_path, rows, cols);
 }
 
 fn validate_args(cli: &Cli) -> Result<(), String> {
@@ -50,10 +65,58 @@ fn validate_args(cli: &Cli) -> Result<(), String> {
     Ok(())
 }
 
-fn main() {
-    let cli = Cli::parse();
+fn split_image(img_path: &PathBuf, rows: u32, cols: u32) -> Vec<DynamicImage> {
+    let mut img = image::open(img_path).unwrap();
+    let mut split_images: Vec<DynamicImage> = Vec::new();
+    let (width, height) = img.dimensions();
 
-    if let Err(err) = validate_args(&cli) {
-        eprintln!("{}", err);
+    let rows = rows.min(height);
+    let cols = cols.min(width);
+
+    for i in 0..rows {
+        split_images.push(img.crop(
+            0,
+            height / rows * i,
+            width,
+            cmp::max(
+                height / rows * (i + 1) - height / rows * i,
+                height - height / rows * i,
+            ),
+        ));
+    }
+
+    for i in 0..split_images.len() {
+        let mut split_image = split_images[i].clone();
+        for j in 0..cols {
+            split_images.push(split_image.crop(
+                width / cols * j,
+                height / rows * i as u32,
+                cmp::max(
+                    width / cols * (j + 1) - width / cols * j,
+                    width - width / cols * j,
+                ),
+                split_image.height(),
+            ));
+        }
+    }
+
+    if cols > 0 {
+        split_images.drain(0..rows as usize);
+    }
+
+    split_images
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_split_image_correct_number_of_images() {
+        let path = &PathBuf::from("./test_assets/16x16.png");
+        assert!(split_image(path, 3, 5).len() == 15);
+        assert!(split_image(path, 1, 1).len() == 1);
+        assert!(split_image(path, 16, 16).len() == 256);
+        assert!(split_image(path, 17, 17).len() == 256);
     }
 }
