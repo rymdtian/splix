d782b79fadb3b03149a820af0d17b5aaa5b76f4e
raymondytian <rymdtian@gmail.com>
2024-04-09 02:34:15 -0500

    feat: user arg for output directory

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,10 @@ struct Cli {
     ///             The first column will take up 2 sections, second column 3 sections, etc.
     #[arg(short, long, value_delimiter = ',', verbatim_doc_comment)]
     cols: Option<Vec<u32>>,
+
+    /// Directory to save the splixed images in. Default: `./splixed-images`.
+    #[arg(short = 'o', long = "output-dir")]
+    output_dir: Option<PathBuf>,
 }
 
 /// Validates the provided command-line arguments.
@@ -171,30 +175,33 @@ fn split_image(img_path: &PathBuf, rows: Vec<u32>, cols: Vec<u32>) -> Vec<Dynami
 /// * `img_format_str` - String representation of the image format.
 fn save_images(
     split_images: &Vec<DynamicImage>,
-    save_directory_str: &str,
+    output_directory: &PathBuf,
     img_format: &ImageFormat,
     img_format_str: &str,
 ) {
-    let mut split_image_path = PathBuf::from(save_directory_str);
-    if let Err(err) = fs::create_dir_all(&split_image_path) {
-        eprintln!(
-            "splix: Failed to create directory {}: {}",
-            save_directory_str, err
-        );
+    let mut output_directory = output_directory.clone();
+    if !output_directory.exists() {
+        if let Err(err) = fs::create_dir_all(&output_directory) {
+            eprintln!(
+                "splix: Failed to create directory {}: {}",
+                output_directory.to_string_lossy(),
+                err
+            );
+        }
     }
-    split_image_path.push("placeholder_filename");
+    output_directory.push("placeholder_filename");
 
     for i in 0..split_images.len() {
-        split_image_path.set_file_name(format!("{}.{}", i, img_format_str));
+        output_directory.set_file_name(format!("{}.{}", i, img_format_str));
 
-        if split_image_path.exists() {
-            if let Err(err) = fs::remove_file(&split_image_path) {
+        if output_directory.exists() {
+            if let Err(err) = fs::remove_file(&output_directory) {
                 eprintln!("Failed to remove existing image {}: {}", i, err);
                 continue; // Skip saving this image if removing the existing one fails
             }
         }
 
-        if let Err(err) = split_images[i].save_with_format(&split_image_path, *img_format) {
+        if let Err(err) = split_images[i].save_with_format(&output_directory, *img_format) {
             eprintln!("splix: Failed to save image #{}: {}", i, err);
         }
     }
@@ -224,10 +231,15 @@ fn main() {
     let img_path = cli.image;
     let rows = cli.rows.unwrap_or(vec![1]);
     let cols = cli.cols.unwrap_or(vec![1]);
-    let save_directory = "split_images"; // TODO: turn to user argument
+    let output_directory = cli.output_dir.unwrap_or(PathBuf::from("splixed-images"));
 
     let split_images = split_image(&img_path, rows, cols);
     let img_format = io::Reader::open(&img_path).unwrap().format().unwrap();
     let img_format_str = img_format.extensions_str()[0];
-    save_images(&split_images, save_directory, &img_format, img_format_str);
+    save_images(
+        &split_images,
+        &output_directory,
+        &img_format,
+        img_format_str,
+    );
 }
